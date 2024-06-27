e98d3ccac0d91a6389cd0a33ccce8dfc0668bea9
M. Freda Antoinette <63315141+fredantoinette@users.noreply.github.com>
2024-06-27 22:41:30 +0100

    Update chapter 5 solution

--- chapter_5/c5-fe4-dictionaries.py
+++ chapter_5/c5-fe4-dictionaries.py
@@ -6,10 +6,9 @@ def get_min(d):
     """d a dict mapping letters to ints
     returns the value in d with the key that occurs first in the alphabet.
     E.g., if d = {x = 11, b = 12}, get_min returns 12."""
-    letters = ('abcdefghijklmnopqrstuvwxyz')
-    for letter in letters:
-        if letter in d:
-            return d[letter]
+    for key, val in d.items():
+        if key == min(d):
+            return val
 
 d = {'x': 11, 'b': 12}
 print(get_min(d))
