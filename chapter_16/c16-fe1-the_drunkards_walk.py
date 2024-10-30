fe2bf36964536d94a2cddb12e81c1d37b3b3f250
M. Freda Antoinette <63315141+fredantoinette@users.noreply.github.com>
2024-10-30 13:57:37 +0000

    Add chapter 16 solution

---
+++ chapter_16/c16-fe1-the_drunkards_walk.py
@@ -0,0 +1,143 @@
+"""
+Write code to produce a plot of the mean distances from the origin, showing 
+distance from starting point versus steps taken. To give a sense of how fast 
+the distance is growing, place on the plot a line showing the square root of 
+the number of steps (and increase the number of steps to 100,000).
+"""
+
+
+# Location and Field classes
+
+class Location(object):
+    def __init__(self, x, y):
+        """x and y are numbers"""
+        self._x, self._y = x, y
+        
+    def move(self, delta_x, delta_y):
+        """delta_x and delta_y are numbers"""
+        return Location(self._x + delta_x, self._y + delta_y)
+    
+    def get_x(self):
+        return self._x
+    
+    def get_y(self):
+        return self._y
+    
+    def dist_from(self, other):
+        ox, oy = other._x, other._y
+        x_dist, y_dist = self._x - ox, self._y - oy
+        return (x_dist**2 + y_dist**2)**0.5
+    
+    def __str__(self):
+        return f"<{self._x}, {self._y}>"
+    
+class Field(object):
+    def __init__(self):
+        self._drunks = {}
+        
+    def add_drunk(self, drunk, loc):
+        if drunk in self._drunks:
+            raise ValueError("Duplicate drunk")
+        else:
+            self._drunks[drunk] = loc
+    
+    def move_drunk(self, drunk):
+        if drunk not in self._drunks:
+            raise ValueError("Drunk not in field")
+        x_dist, y_dist = drunk.take_step()
+        current_location = self._drunks[drunk]
+        # use move method of location to get new location
+        self._drunks[drunk] = current_location.move(x_dist, y_dist)            
+        
+    def get_loc(self, drunk):
+        if drunk not in self._drunks:
+            raise ValueError("Drunk not in field")
+        return self._drunks[drunk]
+    
+    
+import random    
+    
+# Classes defining Drunks
+
+class Drunk(object):
+    def __init__(self, name = None):
+        """Assumes name is a str"""
+        self._name = name
+        
+    def __str__(self):
+        if self != None:
+            return self._name
+        return "Anonymous"
+    
+class Usual_drunk(Drunk):
+    def take_step(self):
+        step_choices  = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        return random.choice(step_choices)
+
+
+import matplotlib.pyplot as plt
+import math
+
+# The drunkard's walk
+
+def walk(f, d, num_steps):
+    """Assumes: f a Field, d a Drunk in f, and num_steps an int >= 0.
+    Moves d num_steps times; returns the distance between the 
+    final location and the location at the start of the walk."""
+    start = f.get_loc(d)
+    for s in range(num_steps):
+        f.move_drunk(d)
+    return start.dist_from(f.get_loc(d))
+
+def sim_walks(num_steps, num_trials, d_class):
+    """Assumes num_steps an int >= 0, num_trials an int > 0,
+    d_class a subclass of Drunk
+    Simulates num_trials walks of num_steps steps each.
+    Returns a list of the final distances for each trial"""
+    Homer = d_class()
+    origin = Location(0, 0)
+    distances = []
+    for t in range(num_trials):
+        f = Field()
+        f.add_drunk(Homer, origin)
+        distances.append(round(walk(f, Homer, num_steps), 1))
+    return distances
+
+def drunk_test(walk_lengths, num_trials, d_class):
+    """Assumes walk_lengths a sequence of ints >= 0
+    num_trials an int > 0, d_class a subclass of Drunk
+    For each number of steps in walk lengths, runs sim_walks with 
+    num_trials walks and prints results"""
+    mean_distances = []
+    sqrt_steps = []
+    for num_steps in walk_lengths:
+        distances = sim_walks(num_steps, num_trials, d_class)
+        print(d_class.__name__, "walk of", num_steps, "steps: Mean =", 
+              f"{sum(distances)/len(distances):.3f}, Max =", 
+              f"{max(distances)}, Min = {min(distances)}")
+        mean_distances.append(sum(distances)/len(distances))
+        sqrt_steps.append(math.sqrt(num_steps))
+    #log10_walk_lengths = [math.log10(x) for x in walk_lengths]
+    #log10_mean_distances = [math.log10(x) for x in mean_distances]
+    #log10_sqrt_steps = [math.log10(x) for x in sqrt_steps]
+    #plt.plot(log10_walk_lengths, log10_mean_distances, "k-", label = d_class.__name__)
+    plt.plot(walk_lengths, mean_distances, "k-", label = d_class.__name__)
+    #plt.plot(log10_walk_lengths, log10_sqrt_steps, "k--", label = "sqrt(steps)")
+    plt.plot(walk_lengths, sqrt_steps, "k--", label = "sqrt(steps)")
+    plt.title(f"Mean Distance from Origin ({num_trials} trials)")
+    plt.xlabel("Number of Steps")
+    plt.ylabel("Distance from Origin")
+    plt.legend(loc = "best")
+    #plt.xticks(log10_walk_lengths, ["${10^1}$", "${10^2}$", "${10^3}$", "${10^4}$", "${10^5}$"])
+    #plt.yticks([1, 2], ["${10^1}$", "${10^2}$"])
+    #plt.semilogx()
+    #plt.semilogy()
+    plt.loglog()
+    plt.show()
+        
+
+# drunk_test((10, 100, 1000, 10000), 100, Usual_drunk)
+
+# drunk_test((0, 1), 100, Usual_drunk)
+
+drunk_test((10, 100, 1000, 10000, 100000), 100, Usual_drunk)
